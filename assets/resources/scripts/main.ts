cb10d5bc233550dff7b7d98538e3432866edb21e
gwxcsny53 <gwxcsny53@163.com>
2025-11-17 17:13:56 +0800

    add borderlight

--- assets/resources/scripts/main.ts
+++ assets/resources/scripts/main.ts
@@ -1,18 +1,21 @@
-import { _decorator, Component, Node, Sprite, tween, UITransform, Vec3 } from 'cc';
+import { _decorator, Component, Node, Sprite, tween, UITransform, Vec2, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('main')
 export class main extends Component {
-
     @property(Node)
     bg: Node = null;
 
     @property(Node)
     rtnode: Node = null;
 
+    @property(Node)
+    borderLightNode: Node = null;
+
     start() {
         this.setRTOffset();
         this.tweBg();
+        this.setBorderLight();
     }
 
     private tweBg() {
@@ -21,7 +24,15 @@ export class main extends Component {
                 tween(this.bg)
                     .to(0.5, { scale: new Vec3(1.2, 1.2, 1.2) })
                     .to(0.5, { scale: Vec3.ONE })
-            ).start()
+            )
+            .start();
+    }
+
+    setBorderLight() {
+        const material = this.borderLightNode.getComponent(Sprite).material;
+        const textureWid = this.borderLightNode.getComponent(UITransform).width;
+        const textureHeight = this.borderLightNode.getComponent(UITransform).height;
+        material.setProperty('textureAspect', new Vec2(textureWid / textureHeight, 1.0));
     }
 
     setRTOffset() {
@@ -32,13 +43,9 @@ export class main extends Component {
         const material = this.rtnode.getComponent(Sprite).material;
         const worldPos = this.rtnode.getWorldPosition();
 
-        material.setProperty("offsetX", worldPos.x - halfX);
-        material.setProperty("offsetY", 720 - worldPos.y - halfY);
+        material.setProperty('offsetX', worldPos.x - halfX);
+        material.setProperty('offsetY', 720 - worldPos.y - halfY);
     }
 
-    update(deltaTime: number) {
-
-    }
+    update(deltaTime: number) {}
 }
-
-
